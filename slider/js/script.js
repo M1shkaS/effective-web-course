cbc720303c4ae69c34054ab1ad7e7b6369376827
Misha <selmisha10@gmail.com>
2022-11-03 11:36:49 +0600

    error correction

--- slider/js/script.js
+++ slider/js/script.js
@@ -2,76 +2,79 @@
 
 let slideIndex = 1;
 
-const slides = document.querySelectorAll('.offer__slide'),
-   prev = document.querySelector('.offer__slider-prev'),
-   next = document.querySelector('.offer__slider-next'),
-   total = document.querySelector('#total'),
-   current = document.querySelector('#current');
+const slides = document.querySelectorAll(".offer__slide"),
+  prev = document.querySelector(".offer__slider-prev"),
+  next = document.querySelector(".offer__slider-next"),
+  total = document.querySelector("#total"),
+  current = document.querySelector("#current");
 
 let currentSlide;
 
-if (localStorage.getItem('currentSlide')) {
-   currentSlide = +localStorage.getItem('currentSlide');
-   slideIndex = currentSlide;
+if (localStorage.getItem("currentSlide")) {
+  currentSlide = +localStorage.getItem("currentSlide");
+  slideIndex = currentSlide;
 } else {
-   localStorage.setItem('currentSlide', slideIndex);
+  localStorage.setItem("currentSlide", slideIndex);
 }
 
 showSlides(slideIndex);
 
 if (slides.length < 10) {
-   total.textContent = `0${slides.length}`;
+  total.textContent = `0${slides.length}`;
 } else {
-   total.textContent = slides.length;
+  total.textContent = slides.length;
 }
 
-function showSlides(n) {
-   if (n > slides.length) {
-      slideIndex = 1;
-   }
-   if (n < 1) {
-      slideIndex = slides.length;
-   }
+// Нажатие кнопок для переключения
+document.addEventListener("keydown", function (event) {
+  if (event.code == "Space") {
+    plusSlides(1);
+  }
 
-   slides.forEach((item) => item.style.display = 'none');
+  if (event.code == "ArrowRight") {
+    plusSlides(1);
+  }
 
-   slides[slideIndex - 1].style.display = 'block';
-
-   if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-   } else {
-      current.textContent = slideIndex;
-   }
-}
-
-function plusSlides(n) {
-   showSlides(slideIndex += n);
-   localStorage.setItem('currentSlide', slideIndex);
-}
-
-document.addEventListener('keydown', function (event) {
-
-   if (event.code == 'Space') {
-      plusSlides(1);
-   }
-
-   if (event.code == "ArrowRight") {
-      plusSlides(1);
-   }
-
-   if (event.code == 'ArrowLeft') {
-      plusSlides(-1);
-   }
+  if (event.code == "ArrowLeft") {
+    plusSlides(-1);
+  }
 });
 
 setInterval(() => {
-   plusSlides(1);
-}, 3000)
+  plusSlides(1);
+}, 2000);
 
-prev.addEventListener('click', function () {
-   plusSlides(-1);
+// Слайд назад
+prev.addEventListener("click", function () {
+  plusSlides(-1);
 });
 
-next.addEventListener('click', function () {
-   plusSlides(1);
+// Слайд вперёд
+next.addEventListener("click", function () {
+  plusSlides(1);
 });
+
+// Показ слайдов
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach((item) => (item.style.display = "none"));
+
+  slides[slideIndex - 1].style.display = "block";
+
+  if (slides.length < 10) {
+    current.textContent = `0${slideIndex}`;
+  } else {
+    current.textContent = slideIndex;
+  }
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  localStorage.setItem("currentSlide", slideIndex);
+}
--- timer/js/script.js
+++ timer/js/script.js
@@ -1,161 +1,156 @@
 // Timer
 
-const btnStart = document.querySelector('.start'),
-   btnStop = document.querySelector('.stop'),
-   btnReset = document.querySelector('.reset'),
-   minutes = document.querySelector('#minutes'),
-   seconds = document.querySelector('#seconds'),
-   body = document.querySelector('body'),
-   btnsMin = document.querySelectorAll('.min button');
+const btnStart = document.querySelector(".start"),
+  btnStop = document.querySelector(".stop"),
+  btnReset = document.querySelector(".reset"),
+  minutes = document.querySelector("#minutes"),
+  seconds = document.querySelector("#seconds"),
+  body = document.querySelector("body"),
+  btnsMin = document.querySelectorAll(".min button");
 
 let timeInterval, minValue, secValue;
 
 const audio = document.querySelector(`audio[data-key='70']`);
 
 // Ограничения на ввод секунд от 0 до 60
-seconds.addEventListener('input', (e)=> {
+seconds.addEventListener("input", (e) => {
+  if (+e.target.value > 60) {
+    seconds.value = 60;
+  }
 
-   if(+e.target.value > 60){
-      seconds.value = 60;
-   }
-
-   if(+e.target.value < 0){
-      seconds.value = 0;
-   }
-})
+  if (+e.target.value < 0) {
+    seconds.value = 0;
+  }
+});
 
 // Перезапуск страницы
-if (localStorage.getItem('minutes')) {
-   minValue = +localStorage.getItem('minutes');
-   secValue = +localStorage.getItem('seconds');
+if (localStorage.getItem("minutes")) {
+  minValue = +localStorage.getItem("minutes");
+  secValue = +localStorage.getItem("seconds");
 
-   if (localStorage.getItem('playing') == 'true') {
+  if (localStorage.getItem("playing") == "true") {
+    minValue = minValue;
+    secValue = secValue;
 
-      minValue = minValue;
-      secValue = secValue;
+    setClock(minValue, secValue);
 
-      setClock(minValue, secValue);
-
-      setDisabled(true);
-   } else {
-      minutes.value = minValue;
-      seconds.value = secValue;
-   }
+    setDisabled(true);
+  } else {
+    minutes.value = minValue;
+    seconds.value = secValue;
+  }
 }
 
 // Кнопки времени
-btnsMin.forEach(btn => {
-   btn.addEventListener('click', (e) => {
-      e.preventDefault();
+btnsMin.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-      min = +e.target.getAttribute('data-min');
-      minutes.value = min;
-      seconds.value = 0;
-   })
-})
+    min = +e.target.getAttribute("data-min");
+    minutes.value = min;
+    seconds.value = 0;
+  });
+});
 
 // Start
-btnStart.addEventListener('click', (e) => {
-   e.preventDefault();
+btnStart.addEventListener("click", (e) => {
+  e.preventDefault();
 
-   minValue = +minutes.value;
-   secValue = +seconds.value;
+  minValue = +minutes.value;
+  secValue = +seconds.value;
 
-   setClock(minValue, secValue);
+  setClock(minValue, secValue);
 
-   setDisabled(true);
+  setDisabled(true);
 });
 
 // Stop
-btnStop.addEventListener('click', (e) => {
-   e.preventDefault();
-   clearInterval(timeInterval);
-   localStorage.setItem('playing', false);
+btnStop.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearInterval(timeInterval);
+  localStorage.setItem("playing", false);
 });
 
 // Reset
-btnReset.addEventListener('click', (e) => {
-   e.preventDefault();
+btnReset.addEventListener("click", (e) => {
+  e.preventDefault();
 
-   clearInterval(timeInterval);
-   minutes.value = 0;
-   seconds.value = 0;
-   localStorage.setItem('minutes', 0);
-   localStorage.setItem('seconds', 0);
+  clearInterval(timeInterval);
+  minutes.value = 0;
+  seconds.value = 0;
+  localStorage.setItem("minutes", 0);
+  localStorage.setItem("seconds", 0);
 
-   body.style.backgroundColor = 'inherit';
-   setDisabled(false);
-   audio.pause();
+  body.style.backgroundColor = "inherit";
+  setDisabled(false);
+  audio.pause();
 });
 
 // Получение времени
 function getTimeRemaining(min, sec) {
-   let seconds = sec;
-   let minutes = min;
+  let seconds = sec;
+  let minutes = min;
 
-   seconds -= 1;
+  seconds -= 1;
 
-   if (seconds <= 0 && minutes != 0) {
-      minutes = minutes - 1;
-      seconds = 60;
-   }
+  if (seconds <= 0 && minutes != 0) {
+    minutes = minutes - 1;
+    seconds = 60;
+  }
 
-   if (seconds <= 0) {
-      seconds = 0;
-   }
-   if (minutes <= 0) {
-      minutes = 0;
-   }
+  if (seconds <= 0) {
+    seconds = 0;
+  }
+  if (minutes <= 0) {
+    minutes = 0;
+  }
 
-   return {
-      'minutes': minutes,
-      'seconds': seconds
-   };
+  return {
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
 
 // Установка времени
 function setClock(minValue, secValue) {
+  timeInterval = setInterval(updateClock, 1000);
 
-   timeInterval = setInterval(updateClock, 1000);
+  updateClock();
 
-   updateClock();
+  function updateClock() {
+    const t = getTimeRemaining(minValue, secValue);
 
-   function updateClock() {
+    minValue = t.minutes;
+    secValue = t.seconds;
 
-      const t = getTimeRemaining(minValue, secValue);
+    localStorage.setItem("minutes", minValue);
+    localStorage.setItem("seconds", secValue);
+    localStorage.setItem("playing", true);
 
-      minValue = t.minutes;
-      secValue = t.seconds;
+    minutes.value = t.minutes;
+    seconds.value = t.seconds;
 
-      localStorage.setItem('minutes', minValue);
-      localStorage.setItem('seconds', secValue);
-      localStorage.setItem('playing', true);
-
-      minutes.value = t.minutes;
-      seconds.value = t.seconds;
-
-      if (t.minutes <= 0 && t.seconds <= 0) {
-         clearInterval(timeInterval);
-         body.style.backgroundColor = 'red';
-         playSound();
-         localStorage.setItem('playing', false);
-
-      }
-   }
+    if (t.minutes <= 0 && t.seconds <= 0) {
+      clearInterval(timeInterval);
+      body.style.backgroundColor = "red";
+      playSound();
+      localStorage.setItem("playing", false);
+    }
+  }
 }
 
 function playSound() {
-   if (!audio) return;
-   audio.currentTime = 0;
-   audio.play();
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function setDisabled(arg) {
-   if (arg) {
-      seconds.setAttribute('disabled', true);
-      minutes.setAttribute('disabled', true);
-   } else {
-      seconds.removeAttribute('disabled');
-      minutes.removeAttribute('disabled');
-   }
+  if (arg) {
+    seconds.setAttribute("disabled", true);
+    minutes.setAttribute("disabled", true);
+  } else {
+    seconds.removeAttribute("disabled");
+    minutes.removeAttribute("disabled");
+  }
 }
