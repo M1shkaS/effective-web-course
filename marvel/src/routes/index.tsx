edb10ab04e39272041f72bce74e6023fcd815887
Misha <selmisha10@gmail.com>
2022-11-07 17:05:04 +0600

    created comics screen

--- marvel/src/routes/index.tsx
+++ marvel/src/routes/index.tsx
@@ -1,10 +1,15 @@
 // В этом файле массив со всем путями / роутами приложения
 import CharactersList from 'components/charactersList/CharactersList';
+import ComicsList from 'components/comicsList/ComicsList';
 
 const routes = [
   {
     path: '/',
     element: <CharactersList />
+  },
+  {
+    path: '/comics',
+    element: <ComicsList />
   },
   { path: '*', element: <div>Not Found</div> }
 ];
